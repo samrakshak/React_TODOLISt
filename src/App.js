14381622ea9973cf79fba1f89790197457c3b082
samrakshak <karkisamrakshak17@gmail.com>
2019-07-12 05:49:33 +0545

    Firts commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoList from './components/TodoList';
+import TodoInput from './components/TodoInput';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid';
+
+class App extends Component {
+  state = {
+    items: [],
+    id: uuid(),
+    item: '',
+    editItem: false
+  };
+
+  handleChange = event => {
+    this.setState({ item: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    };
+
+    const updatedItem = [...this.state.items, newItem];
+    this.setState({
+      items: updatedItem,
+      item: '',
+      id: uuid(),
+      editItem: false
+    });
+  };
+
+  clearList = () => {
+    this.setState({ items: [] });
+  };
+
+  handleDelete = id => {
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({ items: filteredItems });
+  };
+
+  handleEdit = id => {
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+
+    const selectedItem = this.state.items.find(item => item.id == id);
+
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.title,
+      editItem: true,
+      id: id
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-8 mt-4">
+              <h3 className="text-capitalize text-center">TODO Input</h3>
+              <h1>React</h1>
+              <TodoInput
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={this.state.editItem}
+              />
+              <TodoList
+                items={this.state.items}
+                clear={this.clearList}
+                handleDelete={this.handleDelete}
+                handleEdit={this.handleEdit}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
